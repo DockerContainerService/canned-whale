e94707e76d536c36558cf8da634045a10641ea56
Rui Ma <marui_06@outlook.com>
2023-03-23 12:15:47 +0800

    add auth and images file match inner registry url

--- pkg/client/pack/main.go
+++ pkg/client/pack/main.go
@@ -1,11 +1,15 @@
 package pack
 
 import (
+	"fmt"
 	"github.com/AliyunContainerService/image-syncer/pkg/client"
 	"github.com/DockerContainerService/canned-whale/pkg/client/registry"
 	"github.com/DockerContainerService/canned-whale/pkg/utils/path"
 	"github.com/DockerContainerService/canned-whale/pkg/utils/tcp"
 	"github.com/sirupsen/logrus"
+	"os"
+	"path/filepath"
+	"strings"
 	"time"
 )
 
@@ -16,6 +20,9 @@ func RunPackClient(authFile, imageFile string, procNum, retries int, osFilterLis
 	}
 	go registry.RunRegistryClient(listenPort)
 
+	authFile = processSed(authFile, "127.0.0.1", fmt.Sprintf("127.0.0.1:%d", listenPort))
+	imageFile = processSed(imageFile, "127.0.0.1", fmt.Sprintf("127.0.0.1:%d", listenPort))
+
 	for tcp.IsPortAvailable(listenPort) {
 		time.Sleep(1 * time.Second)
 		logrus.Println("waiting for registry up...")
@@ -25,7 +32,7 @@ func RunPackClient(authFile, imageFile string, procNum, retries int, osFilterLis
 		logrus.Fatalf("init PackClient failed: %+v", err)
 	}
 	c.Run()
-	
+
 	registry.StopRegistryClient()
 
 	path.TarPath("/tmp/registry", "registry.tgz")
@@ -33,4 +40,16 @@ func RunPackClient(authFile, imageFile string, procNum, retries int, osFilterLis
 	path.RemovePath("/tmp/registry")
 
 	path.RemovePath("./bin")
+}
+
+func processSed(filePath, src, dst string) (res string) {
+	fo, err := os.ReadFile(filePath)
+	if err != nil {
+		logrus.Fatalf("file %s exchange failed: %+v", filePath, err)
+	}
+	content := strings.ReplaceAll(string(fo), src, dst)
+	res = fmt.Sprintf("./bin/%s", filepath.Base(filePath))
+	fw, err := os.OpenFile(res, os.O_CREATE|os.O_WRONLY, 0666)
+	fw.Write([]byte(content))
+	return res
 }
